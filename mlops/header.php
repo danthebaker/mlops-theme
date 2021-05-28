e31aaae4d556aafb10cf791c0716b7b34f27e166
Kim <kim@bstro.com>
2021-05-27 17:12:12 -0700

    remove conditional in title tag

--- mlops/header.php
+++ mlops/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
+		<title><?php wp_title( '' ); ?></title>
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
