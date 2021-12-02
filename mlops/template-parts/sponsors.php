811862242e07b92ee98abef55cb1e998cb7b5821
Kim <kim@bstro.com>
2021-12-02 11:13:43 -0800

    add datarobot, flyte and arize to sponsor block

--- mlops/template-parts/sponsors.php
+++ mlops/template-parts/sponsors.php
@@ -2,9 +2,12 @@
   <h5>Sponsors</h5>
   <ul class="sponsors-grid">
     <li><a href="https://www.tecton.ai" target="_blank"><img src="/wp-content/themes/mlops/assets/img/tecton-color.png" alt="Tecton logo"></a></li>
-    <li><a href="https://algorithmia.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/algorithmia-250px-wide@2x.png" alt="Algorithmia logo"></a></li>
     <li><a href="https://www.fiddler.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/fiddler-250px-wide.png" alt="Fiddler logo"></a></li>
     <li><a href="https://www.superwise.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/superwise-250px-wide.png" alt="Superwise logo"></a></li>
-    <li><a href="https://www.donut.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/donut_color_150.png" alt="Donut logo"></a></li>    
+    <li><a href="https://www.donut.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/donut_color_150.png" alt="Donut logo"></a></li>
+    
+    <li><a href="https://flyte.org/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/flyte.png" alt="Flyte logo"></a></li>
+    <li><a href="https://www.datarobot.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/datarobot.png" alt="DataRobot logo"></a></li>
+    <li><a href="https://arize.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/logo-arize.png" alt="Arize logo"></a></li>
   </ul>
 </section>
