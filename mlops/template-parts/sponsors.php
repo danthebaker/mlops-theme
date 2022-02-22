63b73e95242c270bfd4d8eb5eefb00c8834c5981
Dan Baker <me@danb.co>
2022-02-22 09:30:57 +0000

    Hack a margin on the logos

--- mlops/template-parts/sponsors.php
+++ mlops/template-parts/sponsors.php
@@ -1,16 +1,16 @@
 <section class="prefooter prefooter--sizemore">
   <h5>Sponsors</h5>
   <ul class="sponsors-grid">
-    <li><a href="https://www.tecton.ai" target="_blank"><img src="/wp-content/themes/mlops/assets/img/tecton-color.png" alt="Tecton logo"></a></li>
-    <li><a href="https://www.fiddler.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/fiddler-250px-wide.png" alt="Fiddler logo"></a></li>
-    <li><a href="https://www.superwise.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/superwise-250px-wide.png" alt="Superwise logo"></a></li>
-    <li><a href="https://www.donut.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/donut_color_150.png" alt="Donut logo"></a></li>
-    <li><a href="https://flyte.org/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/flyte.png" alt="Flyte logo"></a></li>
-    <li><a href="https://www.datarobot.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/datarobot.png" alt="DataRobot logo"></a></li>
-    <li><a href="https://arize.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/logo-arize.png" alt="Arize logo"></a></li>
-    <li><a href="https://www.run.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/runai.png" alt="Run.Ai logo"></a></li>
-    <li><a href="https://redis.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/redis-250px-wide.png" alt="Redis logo"></a></li>
-    <li><a href="https://www.rungalileo.io/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/galileo-250px-wide.png" alt="Galileo logo"></a></li>
-    <li><a href="https://ai-infrastructure.org/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/aiia-250px-wide.png" alt="AIIA logo"></a></li>
+    <li><a href="https://www.tecton.ai" target="_blank"><img src="/wp-content/themes/mlops/assets/img/tecton-color.png" alt="Tecton logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.fiddler.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/fiddler-250px-wide.png" alt="Fiddler logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.superwise.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/superwise-250px-wide.png" alt="Superwise logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.donut.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/donut_color_150.png" alt="Donut logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://flyte.org/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/flyte.png" alt="Flyte logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.datarobot.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/datarobot.png" alt="DataRobot logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://arize.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/logo-arize.png" alt="Arize logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.run.ai/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/runai.png" alt="Run.Ai logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://redis.com/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/redis-250px-wide.png" alt="Redis logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://www.rungalileo.io/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/galileo-250px-wide.png" alt="Galileo logo" style="margin:0 0 20px 0"></a></li>
+    <li><a href="https://ai-infrastructure.org/" target="_blank"><img src="/wp-content/themes/mlops/assets/img/aiia-250px-wide.png" alt="AIIA logo" style="margin:0 0 20px 0"></a></li>
   </ul>
 </section>
